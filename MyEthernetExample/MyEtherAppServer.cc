918e9e8976df982373d6028165124b9c3527bda5
Kibbomi <33482116+Kibbomi@users.noreply.github.com>
2021-04-09 15:21:31 +0900

    Ethernet
    Backup

---
+++ MyEthernetExample/MyEtherAppServer.cc
@@ -0,0 +1,161 @@
+/*
+ * Copyright (C) 2003 Andras Varga; CTIE, Monash University, Australia
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "MyEtherAppServer.h"
+
+#include "inet/applications/ethernet/EtherApp_m.h"
+#include "inet/common/TimeTag_m.h"
+#include "inet/common/ModuleAccess.h"
+#include "inet/common/lifecycle/NodeStatus.h"
+#include "inet/common/packet/Packet.h"
+#include "inet/linklayer/common/Ieee802Ctrl.h"
+#include "inet/linklayer/common/Ieee802SapTag_m.h"
+#include "inet/linklayer/common/MacAddressTag_m.h"
+
+namespace inet {
+
+Define_Module(MyEtherAppServer);
+
+void MyEtherAppServer::initialize(int stage)
+{
+    EV<<" Server Call initialize."<<std::endl;
+    ApplicationBase::initialize(stage);
+
+    if (stage == INITSTAGE_LOCAL) {
+        localSap = par("localSAP");
+
+        // statistics
+        packetsSent = packetsReceived = 0;
+
+        // socket
+        llcSocket.setOutputGate(gate("out"));
+        llcSocket.setCallback(this);
+
+        WATCH(packetsSent);
+        WATCH(packetsReceived);
+    }
+}
+
+void MyEtherAppServer::handleStartOperation(LifecycleOperation *operation)
+{
+    EV<<" Server Call handleStartOperation."<<std::endl;
+    EV_INFO << "Starting application\n";
+    registerDsap(localSap);
+}
+
+void MyEtherAppServer::handleStopOperation(LifecycleOperation *operation)
+{
+    EV<<" Server Call handleStopOperation."<<std::endl;
+    EV_INFO << "Stop the application\n";
+    llcSocket.close();
+    delayActiveOperationFinish(par("stopOperationTimeout"));
+}
+
+void MyEtherAppServer::handleCrashOperation(LifecycleOperation *operation)
+{
+    EV<<" Server Call handleCrashOperation."<<std::endl;
+    EV_INFO << "Crash the application\n";
+    if (operation->getRootModule() != getContainingNode(this))
+        llcSocket.destroy();
+}
+
+void MyEtherAppServer::socketClosed(Ieee8022LlcSocket *socket)
+{
+    EV<<" Server Call socketClosed."<<std::endl;
+    if (operationalState == State::STOPPING_OPERATION && !llcSocket.isOpen())
+        startActiveOperationExtraTimeOrFinish(par("stopOperationExtraTime"));
+}
+
+void MyEtherAppServer::handleMessageWhenUp(cMessage *msg)
+{
+    EV<<" Server Call handleMessageWhenUp."<<std::endl;
+    llcSocket.processMessage(msg);
+}
+
+void MyEtherAppServer::socketDataArrived(Ieee8022LlcSocket*, Packet *msg)
+{
+    EV<<" Server Call socketDataArrived."<<std::endl;
+    EV_INFO << "Received packet `" << msg->getName() << "'\n";
+    //const auto& req = msg->peekAtFront<EtherAppReq>();
+
+    const auto& req = msg->peekAtFront<MyEtherAppReq>();
+    if (req == nullptr)
+        throw cRuntimeError("data type error: not an EtherAppReq arrived in packet %s", msg->str().c_str());
+    packetsReceived++;
+    emit(packetReceivedSignal, msg);
+
+    MacAddress srcAddr = msg->getTag<MacAddressInd>()->getSrcAddress();
+    int srcSap = msg->getTag<Ieee802SapInd>()->getSsap();
+    long requestId = req->getRequestId();
+    long replyBytes = req->getResponseBytes();
+    int repHello = req->getHello() + 1;
+
+    // send back packets asked by EtherAppClient side
+    for (int k = 0; replyBytes > 0; k++) {
+        int l = replyBytes > MAX_REPLY_CHUNK_SIZE ? MAX_REPLY_CHUNK_SIZE : replyBytes;
+        replyBytes -= l;
+
+        std::ostringstream s;
+        s << msg->getName() << "-resp-" << k;
+
+        Packet *outPacket = new Packet(s.str().c_str(), IEEE802CTRL_DATA);
+        const auto& outPayload = makeShared<MyEtherAppResp>();
+        outPayload->setRequestId(requestId);
+        outPayload->setChunkLength(B(l));
+        outPayload->setHello(repHello);
+        outPayload->addTag<CreationTimeTag>()->setCreationTime(simTime());
+        outPacket->insertAtBack(outPayload);
+
+        EV_INFO << "Send response `" << outPacket->getName() << "' to " << srcAddr << " ssap=" << localSap << " dsap=" << srcSap << " length=" << l << "B requestId=" << requestId << "\n";
+        EV_INFO << "Server :  Send Hello Value : "<< outPayload->getHello()<<std::endl;
+        sendPacket(outPacket, srcAddr, srcSap);
+    }
+
+    delete msg;
+}
+
+void MyEtherAppServer::sendPacket(Packet *datapacket, const MacAddress& destAddr, int destSap)
+{
+    EV<<" Server Call sendPacket."<<std::endl;
+    datapacket->addTagIfAbsent<MacAddressReq>()->setDestAddress(destAddr);
+    auto ieee802SapReq = datapacket->addTagIfAbsent<Ieee802SapReq>();
+    ieee802SapReq->setSsap(localSap);
+    ieee802SapReq->setDsap(destSap);
+
+    emit(packetSentSignal, datapacket);
+    llcSocket.send(datapacket);
+    packetsSent++;
+}
+
+void MyEtherAppServer::registerDsap(int dsap)
+{
+    EV<<" Server Call registerDsap."<<std::endl;
+    EV_DEBUG << getFullPath() << " registering DSAP " << dsap << "\n";
+
+    llcSocket.open(-1, dsap);
+}
+
+void MyEtherAppServer::finish()
+{
+}
+
+} // namespace inet
+
